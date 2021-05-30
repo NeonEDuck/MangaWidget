f8ba457f4ca6972fec84fe9b56a156121465d043
NEDuck <cj96cj9696cj@gmail.com>
2021-05-31 05:12:58 +0800

    Add Small and Larget widget support

--- MangaWidget.js
+++ MangaWidget.js
@@ -72,16 +72,50 @@ main: {
       break main
     }
 
+    var mangaList = []
     var showJson = {}
 
-    for (mJson of mangaJson) {  
-      let type = mJson.type.toLowerCase().trim()
-      let link = mJson.link.trim()
-      
-      var manga = await GetMangaChapters(link, type, true)
+    var maxShowNum = config.widgetFamily==="large"?6:config.widgetFamily==="small"?2:3
 
-      showJson[manga.title] = {link: manga.list.link, chapters: manga.list.chapters.slice(0, CHECK_UPDATE_AMOUNT), type: type}
+    switch (config.widgetFamily) {
+      case "large":
+        maxShowNum = 6
+        break
+      case "medium":
+        maxShowNum = 3
+        break
+      case "small":
+        maxShowNum = 2
+        break
+      default:
+        if (args.queryParameters.openMangaList === "true") {
+          maxShowNum = mangaJson.length
+        }
+        else {
+          maxShowNum = 3
+        }
+        break
     }
+
+    var loadManga = async (json, i) => {
+      var type = mJson.type.toLowerCase().trim()
+      var link = mJson.link.trim()
+
+      mangaList[i] = await GetMangaChapters(link, type, true)
+      mangaList[i].type = type
+    }
+    var i = 0
+    var p = []
+
+    for (mJson of mangaJson.slice(0,maxShowNum)) {
+      p.push( loadManga(mJson, i++) )
+    }
+    await Promise.all(p)
+
+    for (manga of mangaList) {
+      showJson[manga.title] = {link: manga.list.link, chapters: manga.list.chapters.slice(0, CHECK_UPDATE_AMOUNT), type: manga.type}
+    }
+
     var oldJson = {}
     
     if (fm.fileExists(path)) {
@@ -97,6 +131,48 @@ main: {
       Script.setWidget(widget)
       
       Script.complete()
+    }
+    else if (args.queryParameters.openMangaList === "true") {
+      var table = new UITable()
+
+      var pivot = false
+
+      var idxJson = []
+
+      for (var title in showJson) {
+        var news = true
+        idxJson.push(showJson[title])
+        idxJson[idxJson.length - 1].title = title
+        if (title in oldJson) {
+          if (oldJson[title].name === showJson[title]["chapters"][0].name) {
+            news = false
+          }
+        }
+
+        var row = new UITableRow()
+        
+        var notify = row.addText("●")
+       
+        if (news) {
+          notify.titleColor = new Color(cGold, 1)
+        }
+        else {
+          notify.titleColor = new Color(cGold, 0)
+        }
+        notify.widthWeight = 1
+
+        row.addText(title).widthWeight = 9
+        row.onSelect = (idx) => {
+          // showMangaChapter(title, chapters[idx], type)
+          xcallback.addParameter("title", idxJson[idx].title)
+          xcallback.addParameter("manga", idxJson[idx].link)
+          xcallback.addParameter("type", idxJson[idx].type)
+          xcallback.open()
+        }
+        table.addRow(row)
+      }
+
+      table.present()
     }
     else {
       createWidget().presentMedium()
@@ -166,27 +242,35 @@ main: {
 //===========================//
 
 function createWidget() {
+  let widgetSize = getWidgetSizeInPoint()
+  let spacerWidth = 5
+  let notifyWidth = 15
+  
   let w = new ListWidget()
   w.backgroundColor = new Color(cBackground)
+  w.url = `scriptable:///run/${encodeURIComponent(Script.name())}?openMangaList=true`
+  
   let s = w.addStack()
   s.layoutHorizontally()
-  s.spacing = 5
+  s.centerAlignContent()
+  s.spacing = spacerWidth
+  s.size = widgetSize
   
   let s1 = s.addStack()
   let spacer = s.addStack()
   let s2 = s.addStack()
   
   spacer.backgroundColor = new Color(cSpacer) 
-  spacer.size = new Size(5, 150)
+  spacer.size = new Size(spacerWidth, widgetSize.height-spacerWidth*2)
   spacer.cornerRadius = 2.5
   
   s1.layoutVertically()
-  s1.size = new Size(25, 150)
-  s1.spacing = 5
+  s1.size = new Size(notifyWidth, widgetSize.height)
+  s1.spacing = spacerWidth
   
   s2.layoutVertically()
-  s2.size = new Size(275, 150)
-  s2.spacing = 5
+  s2.size = new Size(widgetSize.width-(spacerWidth*5+notifyWidth), widgetSize.height)
+  s2.spacing = spacerWidth
   
   for (var title in showJson) {
     var news = 0
@@ -212,23 +296,29 @@ function AddRow(s1, s2, news, title, json, mangalink, type) {
   let ts1 = s1.addStack()
   ts1.layoutVertically()
   ts1.size = new Size(s1.size.width, 0)
-//   AddText(ts1, "●").textColor = new Color(cGold, news?1:0)  
-  let notify = AddText(ts1, String(news))
+
+  let notify = AddText(ts1, (news>Math.min(CHECK_UPDATE_AMOUNT,9)?"+":String(news)))
   notify.textColor = new Color(cGold, (news>0)?1:0)
+  notify.centerAlignText()
   
-  AddText(ts1, "●").textColor = new Color(cGold, 0)
+  AddText(ts1, "0").textColor = new Color(cGold, 0)
   
   let ts2 = s2.addStack()
   ts2.layoutVertically()
   ts2.size = new Size(s2.size.width, 0)
-  let t1 = ts2.addText(title)
-  t1.lineLimit = 1
-  t1.textColor = new Color("#fff", news?1.0:0.5)
-  t1.font = Font.boldSystemFont(17)  
   
-  let t2 = ts2.addText(json.name)
-  t2.lineLimit = 1
-  t2.textColor = new Color("#fff", news?0.9:0.4)  
+  let t1 = ts2.addText(title)
+  t1.lineLimit = 2
+  t1.textColor = new Color("#fff", news?1.0:0.5)
+  t1.font = Font.boldSystemFont(17)
+
+  if (config.widgetFamily !== "small") {
+    t1.lineLimit = 1
+    
+    let t2 = ts2.addText(json.name)
+    t2.lineLimit = 1
+    t2.textColor = new Color("#fff", news?0.9:0.4)
+  }
   
   ts2.url = `scriptable:///run/${encodeURIComponent(Script.name())}?title=${encodeURIComponent(title)}&manga=${encodeURIComponent(mangalink)}&type=${type}`
   
@@ -384,4 +474,49 @@ function DecodeHTMLEntities(text) {
     var num = parseInt(numStr, 10)
     return String.fromCharCode(num)
   })
+}
+
+function getWidgetSizeInPoint(widgetSize = (config.runsInWidget ? config.widgetFamily : "medium")) {
+  // RegExp to verify widgetSize
+  const sizes = /^(?:small|medium|large)$/
+  // stringify device screen size
+  const devSize = (({width: w, height: h}) => w > h ? `${h}x${w}` : `${w}x${h}`)(Device.screenSize())
+  // screen size to widget size mapping for iPhone, excluding the latest iPhone 12 series. iPad size
+  const sizeMap = {
+    // iPad Mini 2/3/4, iPad 3/4, iPad Air 1/2. 9.7" iPad Pro
+    // '768x1024': { small: [0, 0], medium: [0, 0], large: [0, 0] },
+    // 10.2" iPad
+    // '810x1080': { small: [0, 0], medium: [0, 0], large: [0, 0] },
+    // 10.5" iPad Pro, 10.5" iPad Air 3rd Gen
+    // '834x1112': { small: [0, 0], medium: [0, 0], large: [0, 0] },
+    // 10.9" iPad Air 4th Gen
+    // '820x1180': { small: [0, 0], medium: [0, 0], large: [0, 0] },
+    // 11" iPad Pro
+    '834x1194': { small: [155, 155], medium: [329, 155], large: [345, 329] },
+    // 12.9" iPad Pro
+    '1024x1366': { small: [170, 170], medium: [332, 170], large: [382, 332] },
+    // 12 Pro Max
+    // '428x926': { small: [0, 0], medium: [0, 0], large: [0, 0] },
+    // XR, 11, 11 Pro Max
+    '414x896': { small: [169, 169], medium: [360, 169], large: [360, 376] },
+    // 12, 12 Pro
+    // '390x844': { small: [0, 0], medium: [0, 0], large: [0, 0] },
+    // X, XS, 11 Pro, 12 Mini
+    '375x812': { small: [155, 155], medium: [329, 155], large: [329, 345] },
+    // 6/7/8(S) Plus
+    '414x736': { small: [159, 159], medium: [348, 159], large: [348, 357] },
+    // 6/7/8(S) and 2nd Gen SE
+    '375x667': { small: [148, 148], medium: [322, 148], large: [322, 324] },
+    // 1st Gen SE
+    '320x568': { small: [141, 141], medium: [291, 141], large: [291, 299] }
+  }
+  let widgetSizeInPoint = null
+
+  if (widgetSize && sizes.test(widgetSize)) {
+    let mappedSize = sizeMap[devSize]
+    if (mappedSize) {
+      widgetSizeInPoint = new Size(...mappedSize[widgetSize])
+    }
+  }
+  return widgetSizeInPoint
 }
